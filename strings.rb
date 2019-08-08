f6db3288c47ad62ee12fb9706f214c0ba4fafae7
Brandon Allison <brandona8128@outlook.com>
2019-08-08 12:00:02 -0400

    strings.rb

---
+++ strings.rb
@@ -0,0 +1,11 @@
+#working with strings
+#cannot do string interpolation with single quotes
+first_name="Brandon"
+last_name="Allison"
+#concatenation
+puts first_name + " " + last_name
+#string interpolation
+puts "My first name #{first_name} is my last name is #{last_name}"
+full_name = "#{first_name} #{last_name}"
+puts full_name
+#Methods, how to find them
