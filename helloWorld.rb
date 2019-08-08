dc903c42ccbf07900b2ecf576de7f397354cd530
Brandon Allison <brandona8128@outlook.com>
2019-08-08 10:42:39 -0400

    methods

--- helloWorld.rb
+++ helloWorld.rb
@@ -1,1 +1,16 @@
 p "Hello World"
+# p prints to terminal 
+print "Hello World\n"
+#print does not return a new line
+puts "Hello World"
+#put srting 
+greeting = "Hello World"
+p greeting
+#setting a variable to hello world
+#define a method to say hello
+def say_hello(thing_to_say)
+    puts thing_to_say
+end
+
+#calling the method
+say_hello("Hello World")
