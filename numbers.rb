7fd8b82bbbe09590fce498d465d0e132987345c6
Brandon Allison <brandona8128@outlook.com>
2019-09-10 17:05:13 -0400

    added numbers.rb

---
+++ numbers.rb
@@ -0,0 +1,33 @@
+puts 1 + 2
+#integer devision
+
+#convert to float
+
+puts 10.0 / 4
+
+#or
+
+puts 10 / 4.to_f
+
+
+#variables
+x=5
+y=10
+puts y / x
+
+#string times string will not work, string times int gives you the string repeated
+puts "-" * 20
+#times method
+# 20.times{print "-"}
+# 20.times{puts "Hi"}
+# 20.times {puts rand(100)}
+
+#calculator
+puts "Simple Calculator"
+25.times{print "-"}
+puts
+puts "Enter first number"
+num_1 = gets.chomp
+puts "Enter second number"
+num_2 = gets.chomp
+puts "The first number multiplies by the second number is #{num_1.to_i * num_2.to_i}"
--- stringInput.rb
+++ stringInput.rb
@@ -3,14 +3,14 @@ first_name = gets.chomp
 puts "What is your last name"
 last_name = gets.chomp
 full_name = "#{first_name} #{last_name}"
-length = "#{full_name.length()}"
+length = full_name.length()
 puts "Your full name is #{full_name}"
 puts "Your name reverse is #{full_name.reverse()}"
 puts "Your name is #{length} characters long"
 
-# puts "Enter a number to multiply by 2"
-# #get.chomp takes the input
-# input = gets.chomp
-# puts input.to_i * 2;
+puts "Enter a number to multiply by 2"
+#get.chomp takes the input
+input = gets.chomp
+puts input.to_i * 2;
 
 #need to convert input to an integer, otherwise this would print two instances of the number as a string and concatinate it.
