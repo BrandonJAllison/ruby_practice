a30c6b05501c5f97b326cdcb04a79a3a1bc2866b
Brandon Allison <brandona8128@outlook.com>
2019-09-11 07:32:39 -0400

    added more samples

--- ifElseBranching.rb
+++ ifElseBranching.rb
@@ -11,12 +11,36 @@
 
 # conditionals || or && an 
 
-name = 'Tim'
+# name = 'Tim'
 
-if name == "Brandon"
-    puts "Welcome to the program Brandon"
-elsif name == 'Jack'
-puts 'Welcome to the program Jack'
+# if name == "Brandon"
+#     puts "Welcome to the program Brandon"
+# elsif name == 'Jack'
+# puts 'Welcome to the program Jack'
+# else
+#     puts 'Welcome to the program User'
+# end
+
+
+
+puts "What do you want to do? 1) multiply 2) divide 3) subtract 4) find remainder"
+prompt = gets.chomp
+puts "Enter in your first number"
+first_number = gets.chomp
+puts "Enter in your second number"
+second_number = gets.chomp
+if prompt == '1'
+  puts "You have chosen to multiply #{first_number} with #{second_number}"
+  result = multiply(first_number, second_number)
+elsif prompt == '2'
+  puts "You have chosen to divide"
+  result = divide(first_number, second_number)
+elsif prompt == '3'
+  puts "You have chosen to subtract"
+  result = subtract(first_number, second_number)
+elsif prompt == '4'
+  puts "You have chosen to find the remainder"
+  result = mod(first_number, second_number)
 else
-    puts 'Welcome to the program User'
+  puts "You have made an invalid choice"
 end
--- methods.rb
+++ methods.rb
@@ -7,3 +7,19 @@ end
 x = multiply(2,5)
 
 puts x
+
+def multiply(first_number, second_number)
+  first_number.to_f * second_number.to_f      
+end
+ 
+def divide(first_number, second_number)
+  first_number.to_f / second_number.to_f      
+end
+ 
+def subtract(first_number, second_number)
+  second_number.to_f - first_number.to_f      
+end
+ 
+def mod(first_number, second_number)
+  first_number.to_f % second_number.to_f      
+end
