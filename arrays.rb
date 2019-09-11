453276bc27b328ed0187cfce6d511a1d36a9b06f
Brandon Allison <brandona8128@outlook.com>
2019-09-11 08:42:21 -0400

    modified arrays

--- arrays.rb
+++ arrays.rb
@@ -1,3 +1,24 @@
-[1,2,3,4,5,6,7,8]
+a = [1,2,3,4,5,6,7,8]
 
 #array practice
+#ordered list of items/elements
+#maintains an index
+
+puts a
+print a
+p a
+
+#access individual item
+
+p a.last
+
+#array methods
+# shuffle, to_a, 1..100 is a range, reverse, .first, .last, .unshift adds element to front of array, .append adds to end or <<
+# .uniq! will remove duplicates
+# .include checks to see if item is included
+# push and pop adds and removes item
+# lifo "Last in first out"
+# using the ! mutates the caller
+#
+# x = 1..100
+# puts x
