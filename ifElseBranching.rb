086dc3304a7ce65f7ee9f109a2be0574fd045971
Brandon Allison <brandona8128@outlook.com>
2019-09-11 07:29:09 -0400

    Branching

---
+++ ifElseBranching.rb
@@ -0,0 +1,22 @@
+# #if else
+# condition = false
+# another_condition = false
+# if !condition && !another_condition 
+#     puts "This evaluated to true"
+#     #execute
+# else
+#     puts "This evaluated to"
+#     #execute something else
+# end
+
+# conditionals || or && an 
+
+name = 'Tim'
+
+if name == "Brandon"
+    puts "Welcome to the program Brandon"
+elsif name == 'Jack'
+puts 'Welcome to the program Jack'
+else
+    puts 'Welcome to the program User'
+end
---
+++ methods.rb
@@ -0,0 +1,9 @@
+#methods need to be defined befre execution call
+
+def multiply (a,b)
+a.to_f * b.to_f #return is automatically implied or return
+end
+
+x = multiply(2,5)
+
+puts x
